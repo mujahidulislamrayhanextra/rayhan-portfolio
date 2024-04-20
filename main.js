d2df95de64b3835f6b4048933088bc19efad772b
mujahidulislamrayhanextra <mujahidulislamrayhanextra@gmail.com>
2024-04-20 23:53:37 +0600

    hi

---
+++ main.js
@@ -0,0 +1,7 @@
+new Typed('.text', {
+    strings: ['Fullstack Developer,', 'Welcome to my website!', 'Thank you for visiting.'],
+    typeSpeed: 100,
+    backspeed: 100,
+    backDelay:1000,
+    loop: true
+    });
