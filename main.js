f60384c083b77f8c1de19645df5dccc87f164a79
mujahidulislamrayhanextra <mujahidulislamrayhanextra@gmail.com>
2024-04-25 20:28:18 +0600

    fff

--- main.js
+++ main.js
@@ -5,3 +5,30 @@ new Typed('.text', {
     backDelay:1000,
     loop: true
     });
+
+    let number = document.getElementById("number");
+    let counting = 0;
+    setInterval(() => {
+        if (counting == 80 ) {
+            clearInterval
+            
+        }
+        else{
+            counting += 1;
+            number.innerHTML = counting + "%";
+        }
+    
+    },25);
+    let number1 = document.getElementById("number1");
+    let counting1 = 0;
+    setInterval(() => {
+        if (counting1 == 80 ) {
+            clearInterval
+            
+        }
+        else{
+            counting1 += 1;
+            number1.innerHTML = counting1 + "%";
+        }
+    
+    },25);
